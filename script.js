eb36e9fb85cf1e779671d91beacd31c2e6f82f19
grandmastar27 <grandmastar27@gmail.com>
2020-03-21 08:30:16 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
     var counter = 0;
     $('img').fadeIn(2000);
+    $('img').attr('draggable', false);
     $(".cookieClick").click(function(){
         counter++;
 
